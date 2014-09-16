e63f00169bc7ff8282c54d50c03f12ce2a605d4a
Charmaine <Charmaine@172.28.188.100>
2014-09-16 10:26:14 +0800

    commmit PurchaseOrderEntity

--- IslandFurni-ejb/src/java/entity/PurchaseOrderEntity.java
+++ IslandFurni-ejb/src/java/entity/PurchaseOrderEntity.java
@@ -7,16 +7,16 @@
 package entity;
 
 import java.io.Serializable;
-import java.sql.Date;
-import java.sql.Time;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 
 /**
  *
@@ -26,31 +26,46 @@ import javax.persistence.ManyToMany;
 public class PurchaseOrderEntity implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long purchaseID;
     private String type;
-    private Date purchaseDate;
-    private Time purchaseTime;
+    private String purchaseDate;
+    private String purchaseTime;
     private Long sendFrom;
-    private Long sentTo;
-    private int quantity;
+    private Long sendTo;
+    private List quantity;
     private double cost;
     private String status;
     
-    @ManyToMany
+    @ManyToMany(cascade={CascadeType.ALL})
     @JoinTable(name="PurchaseOrderEntity_RawMaterialEntity")
-    private Set<RawMaterialEntity> rawMats = new HashSet<RawMaterialEntity>();
+    private List<RawMaterialEntity> parts = new ArrayList<>();
     
-    @ManyToMany
+    @ManyToMany(cascade={CascadeType.ALL})
     @JoinTable(name="PurchaseOrderEntity_FinishedGoodEntity")
-    private Set<FinishedGoodEntity> finishedGoods = new HashSet<FinishedGoodEntity>();
+    private List<FinishedGoodEntity> finishedGoods = new ArrayList<>();
 
-    public Long getId() {
-        return id;
+    @ManyToOne(cascade={CascadeType.ALL})
+    private ManuFacEntity manuFac = new ManuFacEntity();
+
+    public PurchaseOrderEntity() {
+    }
+    
+    public void create(String type, String purchaseDate, String purchaseTime, Long sendFrom, Long sendTo, String status) {
+        this.setType(type);
+        this.setPurchaseDate(purchaseDate);
+        this.setPurchaseTime(purchaseTime);
+        this.setSendFrom(sendFrom);
+        this.setSendTo(sendTo); 
+        this.setStatus(status);
+    }
+    
+    public Long getPurchaseID() {
+        return purchaseID;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setPurchaseID(Long purchaseID) {
+        this.purchaseID = purchaseID;
     }
 
     public String getType() {
@@ -61,19 +76,19 @@ public class PurchaseOrderEntity implements Serializable {
         this.type = type;
     }
 
-    public Date getPurchaseDate() {
+    public String getPurchaseDate() {
         return purchaseDate;
     }
 
-    public void setPurchaseDate(Date purchaseDate) {
+    public void setPurchaseDate(String purchaseDate) {
         this.purchaseDate = purchaseDate;
     }
 
-    public Time getPurchaseTime() {
+    public String getPurchaseTime() {
         return purchaseTime;
     }
 
-    public void setPurchaseTime(Time purchaseTime) {
+    public void setPurchaseTime(String purchaseTime) {
         this.purchaseTime = purchaseTime;
     }
 
@@ -85,19 +100,19 @@ public class PurchaseOrderEntity implements Serializable {
         this.sendFrom = sendFrom;
     }
 
-    public Long getSentTo() {
-        return sentTo;
+    public Long getSendTo() {
+        return sendTo;
     }
 
-    public void setSentTo(Long sentTo) {
-        this.sentTo = sentTo;
+    public void setSendTo(Long sendTo) {
+        this.sendTo = sendTo;
     }
 
-    public int getQuantity() {
+    public List getQuantity() {
         return quantity;
     }
 
-    public void setQuantity(int quantity) {
+    public void setQuantity(List quantity) {
         this.quantity = quantity;
     }
 
@@ -110,35 +125,42 @@ public class PurchaseOrderEntity implements Serializable {
     }
 
     public String getStatus() {
+        status = "Pending";
         return status;
     }
 
     public void setStatus(String status) {
         this.status = status;
     }
-
-    public Set<RawMaterialEntity> getRawMats() {
-        return rawMats;
+    
+    public List<RawMaterialEntity> getParts() {
+        return parts;
     }
 
-    public void setRawMats(Set<RawMaterialEntity> rawMats) {
-        this.rawMats = rawMats;
+    public void setParts(List<RawMaterialEntity> parts) {
+        this.parts = parts;
     }
 
-    public Set<FinishedGoodEntity> getFinishedGoods() {
+    public List<FinishedGoodEntity> getFinishedGoods() {
         return finishedGoods;
     }
 
-    public void setFinishedGoods(Set<FinishedGoodEntity> finishedGoods) {
+    public void setFinishedGoods(List<FinishedGoodEntity> finishedGoods) {
         this.finishedGoods = finishedGoods;
     }
 
-    
+    public ManuFacEntity getManuFac() {
+        return manuFac;
+    }
+
+    public void setManuFac(ManuFacEntity manuFac) {
+        this.manuFac = manuFac;
+    } 
 
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
+        hash += (purchaseID != null ? purchaseID.hashCode() : 0);
         return hash;
     }
 
@@ -149,7 +171,7 @@ public class PurchaseOrderEntity implements Serializable {
             return false;
         }
         PurchaseOrderEntity other = (PurchaseOrderEntity) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+        if ((this.purchaseID == null && other.purchaseID != null) || (this.purchaseID != null && !this.purchaseID.equals(other.purchaseID))) {
             return false;
         }
         return true;
@@ -157,7 +179,7 @@ public class PurchaseOrderEntity implements Serializable {
 
     @Override
     public String toString() {
-        return "entity.PurchaseOrderEntity[ id=" + id + " ]";
+        return "entity.PurchaseOrderEntity[ id=" + purchaseID + " ]";
     }
     
 }
